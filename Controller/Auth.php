eaaa8bf75ed475c8e803ca1e575033983d1278c1
shien-p15v3 <img21326@gmail.com>
2016-12-07 13:24:49 +0800

    新增class Auth來使用物件導向會員系統

---
+++ Controller/Auth.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: shien
+ * Date: 2016/12/7
+ * Time: 下午 1:19
+ */
+class Auth
+{
+    public function register($request){ //註冊
+        $sql="INSERT INTO `contact` (`SID`, `num_txt`, `password`, `name`, `phone`, `email`, `FID`, `group_id`, `sday`, `status`) VALUES ('".$request['SID']."', '".$request['numtxt']."', '".$request['password']."','".$request['name']."', '".$request['phone']."', '".$request['FID']."', '".$request['group_id']."', '".$request['sday']."', '".$request['status']."');";
+        if(mysqli_query($sql)){
+            return '成功';
+        }else{
+            return '失敗';
+        }
+    }
+
+    public function login($request){    //登入
+
+    }
+}
