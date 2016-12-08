119807afc05da54f991f79144da84b8959ea0676
shien.school <img21326@gmail.com>
2016-12-08 15:53:16 +0800

    add createPost finish

--- Controller/Post.php
+++ Controller/Post.php
@@ -17,15 +17,26 @@ class Post
     public function createPost($requset){   // 新增動態消息
         $mysqli = Connect::conn();
         $contact = $requset['contact'];
-        $group = (isset( $requset['contact'])) ? ( $requset['contact']) : ("NULL");
+        $group = (isset( $requset['group'])) ? ( $requset['group']) : ("NULL");
         $sql = "INSERT INTO post (post_id, SID, contact, post_time, group_id)";
-        $sql = sql . " VALUES (NULL, '".$this->sid."', '".$contact."', CURRENT_TIMESTAMP, ".$group.");";
-        if (!$mysqli->query($sql)) {  //讀取錯誤訊息
+        $sql = $sql . " VALUES (NULL, '".$this->sid."', '".$contact."', CURRENT_TIMESTAMP, '".$group."');";
+        if (!$mysqli->query($sql)) {  //讀取錯誤訊息&&傳送資料
             printf("Errormessage: %s\n", $mysqli->error);
         }else{
             return '成功';
         }
         unset($sql);
         $mysqli->close();
+//        $post = new Post();
+//        $arr = [
+//            "contact" => "hi~",
+//            "group" =>  "1",
+//        ];
+//        $post->createPost($arr);
+    }
+
+    public static function deletePost($id){
+        $sql = SELECT * FROM `post` WHERE `SID` = $id
+        $sql = "DELETE FROM `post` WHERE `post`.`post_id` = 6";
     }
 }
