5342fc29f7009c4fc37b862aba4919d0303cea8b
shien.school <img21326@gmail.com>
2016-12-08 15:39:33 +0800

    post -> CreatePost

--- Controller/Post.php
+++ Controller/Post.php
@@ -8,5 +8,24 @@
  */
 class Post
 {
+    public $sid;
+    public function __construct()
+    {
+        $this->sid = $_SESSION['sid'];
+    }
 
+    public function createPost($requset){   // 新增動態消息
+        $mysqli = Connect::conn();
+        $contact = $requset['contact'];
+        $group = (isset( $requset['contact'])) ? ( $requset['contact']) : ("NULL");
+        $sql = "INSERT INTO post (post_id, SID, contact, post_time, group_id)";
+        $sql = sql . " VALUES (NULL, '".$this->sid."', '".$contact."', CURRENT_TIMESTAMP, ".$group.");";
+        if (!$mysqli->query($sql)) {  //讀取錯誤訊息
+            printf("Errormessage: %s\n", $mysqli->error);
+        }else{
+            return '成功';
+        }
+        unset($sql);
+        $mysqli->close();
+    }
 }
