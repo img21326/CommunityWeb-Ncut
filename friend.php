1534aec4e95ba460f33dc8fffba3138ac910d50b
shien-p15v3 <img21326@gmail.com>
2016-12-11 16:09:55 +0800

    OK

--- friend.php
+++ friend.php
@@ -47,7 +47,7 @@ if(!$s){
 
             </div>
             <div class="col-md-offset-3 col-md-2 ko" qw="好朋友">
-                <a href="friend.php"><img src="images/user.png" style="max-width: 80px;"></a>
+                <img src="images/user.png" style="max-width: 80px;">
             </div>
             <div class="col-md-2 ko" qw="群組">
                 <img src="images/team.png" style="max-width: 80px;">
