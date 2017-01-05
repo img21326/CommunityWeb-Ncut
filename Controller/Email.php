7541bbb82ca0aa5d8a3ae32a4be5656062fcfd6a
煞氣a冠誠 <q2404224u@gmail.com>
2017-01-05 17:11:07 +0800

    ''

---
+++ Controller/Email.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/1/5
+ * Time: 下午 03:56
+ */
+class Email
+{
+
+}
---
+++ Email.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: admin
+ * Date: 2017/1/5
+ * Time: 下午 04:07
+ */
